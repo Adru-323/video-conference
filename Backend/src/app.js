e27d3f41c88cb6f96b9bce571b5a0a0adcc53076
Adarsh Salunkhe <adarshsalunkhe19089@gmail.com>
2025-11-15 20:50:32 +0530

    Modify CORS settings in app.js
    Updated CORS configuration to allow specific methods and origins.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,12 @@ const io = connectToSocket(server);
 
 
 app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+// app.use(cors());  modified
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  credentials: false
+}));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
