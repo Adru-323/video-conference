c27a4b21dbc2b6e7c8620d65f7f0f74db89fa99a
Adarsh Salunkhe <adarshsalunkhe19089@gmail.com>
2025-11-15 20:38:12 +0530

    Update environment.js

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,9 +1,7 @@
 let IS_PROD = true;
 
+const server = IS_PROD
+  ? "https://video-conference-jt4z.onrender.com"
+  : "http://localhost:8000";
 
-const server =  IS_PROD ? 
-    "https://video-conference-jt4z.onrender.com" :
-     ""http://localhost:8000""
-
-// http://localhost:8000
 export default server;
